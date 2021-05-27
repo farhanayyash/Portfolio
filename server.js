e466cd0513c8d1875f0f05e1d32a4ff153511a41
farhanayyash <farhanayyash16@yahoo.com>
2021-05-27 12:54:04 +0300

    fix

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var nodemailer = require('nodemailer');
 const server = express();
 var bodyParser = require('body-parser');
 
+var XOAuth2 = require('xoauth2');
+
+
 const PORT = process.env.PORT || 3000 ;
 
 server.use(bodyParser.urlencoded({ extended: true })); 
@@ -16,16 +19,45 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.listen(PORT,()=>{
   console.log('listening to port:'+PORT);
 })
+var smtpTransport = nodemailer.createTransport(process.env.DB_USER3+encodeURIComponent(process.env.DB_PASS) + "@smtp.gmail.com:465"); 
 
 
-var transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS
-  }
-});
+// var smtpTransport = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//       xoauth2: xoauth2.createXOAuth2Generator({
+//           user: process.env.DB_USER,
+//           clientId: process.env.clientId,
+//           clientSecret: process.env.clientSecret,
+//           refreshToken: process.env.refreshToken
+//       })
+//   }
+// });
 
+// var transporter = nodemailer.createTransport({
+//   service: 'Gmail',
+//   auth: {
+//     user: process.env.DB_USER,
+//     pass: process.env.DB_PASS
+//   }
+// });
+
+
+
+///////////////////
+
+
+// var smtpTransport = nodemailer.createTransport("SMTP", {
+//   service: "Gmail",
+//   auth: {
+//     XOAuth2: {
+//       user: process.env.DB_USER,                        
+//       clientId: process.env.clientId,
+//       clientSecret: process.env.clientSecret,
+//       refreshToken: process.env.refreshToken
+//     }
+//   }
+// });
 
 server.get('/test',(req,res)=>{
   res.send("my server work");
@@ -34,22 +66,35 @@ server.use(express.static('./public'));
 
 
 server.post('/', function(req, res){
+
   console.log(req.body);
   // res.send("recieved your request!");
   var mforme = "UserName: "+req.body.name+" \nEmail: "+req.body.email+"\nmessage: "+req.body.message;
-  var mailOptions = {
+  var mailOptions  = {
     from: process.env.DB_USER,
     to: process.env.DB_USER2,
     subject: 'Sending Email From My Website',
     text: mforme
   };
   
-  transporter.sendMail(mailOptions, function(error, info){
+
+
+  smtpTransport.sendMail(mailOptions, function(error, response) {
     if (error) {
       console.log(error);
     } else {
-      console.log('Email sent: ' + info.response);
+      console.log(response);
     }
   });
+
+
+
+  // transporter.sendMail(mailOptions, function(error, info){
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     console.log('Email sent: ' + info.response);
+  //   }
+  // });
   res.redirect('back.html');
 });
